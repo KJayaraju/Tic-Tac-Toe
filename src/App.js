d1e209be92c1e337d65eec938f5d2c0d18bd87ce
KJayaraju <146174744+KJayaraju@users.noreply.github.com>
2024-10-04 18:59:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import {Component} from 'react'
+import TicTacToe from './component/Userprofile';
+
+class App extends Component{
+    render(){
+        return <TicTacToe /> 
+    }
+}
+export default App;
---
+++ src/inde.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+
+
+
+const Userprofile=(props)=>{
+    const {userDeatils}=props
+    const {imageurl}=userDeatils
+    return(
+    <div>
+        <img src="https://tse4.mm.bing.net/th?id=OIP.rvSWtRd_oPRTwDoTCmkP5gHaE8&pid=" className='avater'/>
+    </div>
+)}
+export default Userprofile
